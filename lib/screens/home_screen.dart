3ba09f86988e43bf0e2041f208c4c65213b4eae8
ilhamimetin <imetin86@gmail.com>
2024-12-23 02:46:45 +0300

    Veri yükleme işlevi eklendi ve gerekli dosya yolu güncellendi.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,21 +1,36 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  void _loadData() async {
+    // Burada veri çekme işlemleri yapılacak
+    final dataString = await rootBundle.loadString("assets/files/data.json");
+    final dataJson = jsonDecode(dataString);
+    print(dataJson["categories"]);
+    // debugPrint(dataJson.runtimeType.toString());
+  }
+
+  @override
+  void initState() {
+    _loadData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
-          children: [
-            Text("Deneme")
-          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text("Deneme")],
         ),
       ),
     );
