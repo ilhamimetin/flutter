0ac19b96d64de5a52837344bf6e2db1371e0ae38
ilhamimetin <imetin86@gmail.com>
2024-12-23 01:13:20 +0300

    json_islemleri_start

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,19 +8,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List? veriler;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (veriler == null) Text("veri yok"),
-            if (veriler != null) Text(veriler![0]),
-            veriler == null ? Text("Veri Yok") : Text(veriler![0])
-          ],
+          children: [],
         ),
       ),
     );
