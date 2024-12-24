dbd718516b8efc2202089717726a3a741a5cab9e
ilhamimetin <imetin86@gmail.com>
2024-12-24 14:29:40 +0300

    json işlemleri devam

--- lib/models/elemanlar_model.dart
+++ lib/models/elemanlar_model.dart
@@ -1,7 +1,40 @@
+class Urun {
+  final int id;
+  final int kategori;
+  final String isim;
+  final String resim;
+
+  Urun(this.id, this.kategori, this.isim, this.resim);
+
+  factory Urun.fromJson(Map<String, dynamic> json) {
+    return Urun(json["idi"], json["kategori"], json["isim"], json["resim"]);
+  }
+}
+
+class Kategori {
+  final int id;
+  final String kategori;
+
+  Kategori(this.id, this.kategori);
+
+  factory Kategori.fromJson(Map<String, dynamic> json) {
+    return Kategori(json["id"], json["kategori"]);
+  }
+}
+
 class ElemanlarModel {
-  final String title;
-  final String subTitle;
+  final List<Urun> urunler;
+  final List<Kategori> kategoriler;
 
-  ElemanlarModel(this.title, this.subTitle);
+  ElemanlarModel(this.urunler, this.kategoriler);
 
+  factory ElemanlarModel.fromJson(Map<String, dynamic> json) {
+    var urunlerList = json["urunler"] as List;
+    var kategorilerList = json["kategoriler"] as List;
+
+    List<Urun> urunler = urunlerList.map((urun) => Urun.fromJson(urun)).toList();
+    List<Kategori> kategoriler =kategorilerList.map((kategori) => Kategori.fromJson(kategori)).toList();
+
+    return ElemanlarModel(urunler, kategoriler);
+  }
 }
