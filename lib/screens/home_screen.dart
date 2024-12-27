8daae0cfc384541e9f713beab210417364a9a583
ilhamimetin <imetin86@gmail.com>
2024-12-27 14:24:41 +0300

    23_yeni

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,4 @@
-
 import 'package:flutter/material.dart';
-
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,14 +6,26 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {  
-
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        
+        child: Container(
+          height: 200,
+          color: Colors.grey,
+          child: Padding(padding: EdgeInsets.all(20),
+          child: Wrap(
+            children: [
+                _ornekWidget(),
+            ],
+          ),),
+        ),
       ),
     );
+  }
+  
+  Container _ornekWidget() {
+    
   }
 }
