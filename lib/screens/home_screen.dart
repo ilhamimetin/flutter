c74ac461fa2fc2cfcff3216f4e43cb2ba08ed600
ilhamimetin <imetin86@gmail.com>
2024-12-27 21:13:41 +0300

    Açıklamalı Expanded Flexible

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,51 +10,29 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          height: 200,
-          color: Colors.grey,
-          child: Padding(
-            padding: EdgeInsets.all(20),
-            child: /*  Row */ Wrap(
-              // Burada Row kullanımında taşma hatası verir. Aynı şey column için de geçerlidir. Bu hatayı aşmak için Wrap Widget'i kullanılır. Spacing ve runSpacing ile widget'lar arasında boşluk bıraklabilir.
-              spacing: 10,
-              runSpacing: 10,
-              direction: Axis.vertical, // yukarıdan aşağıya hizalar
-              children: [
-                _ornekWidget(),
-                _ornekWidget(),
-                _ornekWidget(),
-                _ornekWidget(),
-                _ornekWidget(),
-                _ornekWidget(),
-                _ornekWidget(),
-                _ornekWidget(),
-                _ornekWidget(),
-              ],
-            ),
+        body: Column(
+      children: [
+        Flexible(
+          flex: 2,
+          // Kendi boyutunu olduğunca küçültmeye çalışır içindeki değere bakmaksızın.
+          child: Container(
+            height: 200,
+            color: Colors.redAccent,
           ),
         ),
-      ),
-    );
-  }
-
-  Container _ornekWidget() {
-    return Container(
-      width: 100,
-      height: 30,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Colors.greenAccent,
-      ),
-      child: const Text(
-        'Item',
-        style: TextStyle(
-          fontSize: 15,
-          fontWeight: FontWeight.w500,
+        Expanded(
+          flex: 3,
+          // Expanded içindeki column'un yükseklik değeriine bakmaksızın ekrana kaplayamaya çalışır.
+          child: Container(
+            height: 100,
+            color: Colors.blueGrey.shade200,
+          ),
         ),
-      ),
-    );
+        Container(
+          height: 100,
+          color: Colors.lightBlue,
+        )
+      ],
+    ));
   }
 }
